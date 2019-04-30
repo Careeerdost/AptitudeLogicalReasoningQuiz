1cbda2336d0815502767ca6ef3b4671235196355
Careerdost Enterprise <rohitsha@gmail.com>
2019-04-30 19:21:41 +0530

    Update quiz_category.java

--- app/src/main/java/in/careerdost/quiznew/activities/quiz_category.java
+++ app/src/main/java/in/careerdost/quiznew/activities/quiz_category.java
@@ -13,8 +13,6 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.daimajia.androidanimations.library.Techniques;
-import com.daimajia.androidanimations.library.YoYo;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -45,30 +43,21 @@ public class quiz_category extends AppCompatActivity {
     private Question_easy currentQ;
     private TextView question, timer, overallScore, txtTotalQuestion,
             txtShowAnswer, overallCorrectAnswer;
-    private Button btnHint, btnConfirm;
+    private Button btnConfirm;
     private RadioGroup rbGroup;
     private RadioButton rb1, rb2, rb3, rb4;
 
     private int skipped = 0;
-    /*private int incorrectAnswer = totalQuestion - (score / 5);
-    int eachCorrectAnswerScore = 5;
-    int eachIncorrectAnswerScore = 0;
-    int correctAnswers = totalQuestion - incorrectAnswer;*/
-
     private ColorStateList txtColorDefaultRb;
     private ColorStateList txtColorDefaultCd;
     private long timeLeftInMillis;
     private boolean answered;
-
     private long backPressedTime;
-    Aptitude_cal_db aptitude_cal_db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz_category);
-
-        aptitude_cal_db = new Aptitude_cal_db(this);
 
         question = findViewById(R.id.question);
         rbGroup = findViewById(R.id.radio_group);
@@ -77,25 +66,17 @@ public class quiz_category extends AppCompatActivity {
         rb3 = findViewById(R.id.radio_button3);
         rb4 = findViewById(R.id.radio_button4);
 
-        Button btnShareQues = findViewById(R.id.btn_share_ques);
-        btnHint = findViewById(R.id.btn_hint);
         btnConfirm = findViewById(R.id.btn_confirm);
         Button btnSkip = findViewById(R.id.btn_skip);
-
         txtShowAnswer = findViewById(R.id.txt_show_Answer);
         TextView catLevel = findViewById(R.id.ques_category);
         overallCorrectAnswer = findViewById(R.id.overallCorrectAnswer);
-
         overallScore = findViewById(R.id.overallScore);
         timer = findViewById(R.id.timer);
         txtTotalQuestion = findViewById(R.id.txtTotalQuestion);
 
-        txtColorDefaultRb = rb1.getTextColors();
-        txtColorDefaultCd = timer.getTextColors();
-
         Intent intent = getIntent();
         String easyCategory = intent.getStringExtra(start_quiz.EASY_CATEGORY);
-
         catLevel.setText(easyCategory);
 
         overallScore.setText(getResources().getString(R.string.score_zero));
@@ -113,17 +94,6 @@ public class quiz_category extends AppCompatActivity {
         timeLeftInMillis = COUNTDOWN_IN_MILLIS;
         startCountDown();
 
-        btnShareQues.setOnClickListener((View v) -> {
-            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-            sharingIntent.setType("text/plain");
-            String shareBody = "Can you answer this?\n" + currentQ.getQUESTION() +
-                    "\n\n- via @careerdost #test #quiz - FREE #Android #app Download now!" +
-                    "\nhttps://bit.ly/2G0Z78s";
-            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Question_easy of the Day!");
-            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-            startActivity(Intent.createChooser(sharingIntent, "Share via"));
-        });
-
         btnConfirm.setOnClickListener((View v) -> {
             if (!answered) {
                 if (rb1.isChecked() || rb2.isChecked() || rb3.isChecked() || rb4.isChecked()) {
@@ -139,23 +109,6 @@ public class quiz_category extends AppCompatActivity {
             skipped = skipped + 1;
             setNextQues();
         });
-        btnHint.setOnClickListener((View v) -> {
-            attempt_counter--;
-            if (attempt_counter > 0) {
-                Toast.makeText(quiz_category.this, "Max Hints = 6\nHints Left = " + attempt_counter, Toast.LENGTH_SHORT).show();
-                YoYo.with(Techniques.StandUp)
-                        .duration(700)
-                        .playOn(findViewById(R.id.linear_show_answer));
-                txtShowAnswer.setText(currentQ.getSOLVED());
-            } else {
-                //attempt_counter--;
-                if (attempt_counter == 0) {
-                    Toast.makeText(quiz_category.this, "Sorry, max Hints used", Toast.LENGTH_SHORT).show();
-                    btnHint.setEnabled(false);
-                    btnHint.setAlpha(.5f);
-                }
-            }
-        });
 
         AdView mAdView = findViewById(R.id.quiz_banner_ad);
         AdRequest adRequest = new AdRequest.Builder().build();
@@ -168,25 +121,6 @@ public class quiz_category extends AppCompatActivity {
     }
 
     private void setNextQues() {
-        YoYo.with(Techniques.SlideInDown)
-                .duration(500)
-                .playOn(findViewById(R.id.question));
-        rb1.setTextColor(txtColorDefaultRb);
-        YoYo.with(Techniques.SlideInLeft)
-                .duration(700)
-                .playOn(findViewById(R.id.radio_button1));
-        rb2.setTextColor(txtColorDefaultRb);
-        YoYo.with(Techniques.SlideInRight)
-                .duration(800)
-                .playOn(findViewById(R.id.radio_button2));
-        rb3.setTextColor(txtColorDefaultRb);
-        YoYo.with(Techniques.SlideInLeft)
-                .duration(900)
-                .playOn(findViewById(R.id.radio_button3));
-        rb4.setTextColor(txtColorDefaultRb);
-        YoYo.with(Techniques.SlideInRight)
-                .duration(1000)
-                .playOn(findViewById(R.id.radio_button4));
         rbGroup.clearCheck();
         txtShowAnswer.setText("");
         if (qid < totalQuestion) {
@@ -208,39 +142,8 @@ public class quiz_category extends AppCompatActivity {
         }
     }
 
-    private void startCountDown() {
-        CountDownTimer countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                timeLeftInMillis = millisUntilFinished;
-                updateCountDownText();
-            }
-
-            @Override
-            public void onFinish() {
-                timeLeftInMillis = 0;
-                updateCountDownText();
-                checkAnswer();
-            }
-        }.start();
-    }
-
-    private void updateCountDownText() {
-        int minutes = (int) (timeLeftInMillis / 1000) / 60;
-        int seconds = (int) (timeLeftInMillis / 1000) % 60;
-
-        String timeFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
-        timer.setText(timeFormatted);
-        if (timeLeftInMillis < 30000) {
-            timer.setTextColor(Color.RED);
-        } else {
-            timer.setTextColor(txtColorDefaultCd);
-        }
-    }
-
     private void checkAnswer() {
         answered = true;
-
         RadioButton rbSelected = findViewById(rbGroup.getCheckedRadioButtonId());
         int answerNr = rbGroup.indexOfChild(rbSelected) + 1;
 
@@ -264,30 +167,18 @@ public class quiz_category extends AppCompatActivity {
 
         switch (currentQ.getANSWER()) {
             case 1:
-                YoYo.with(Techniques.RollIn)
-                        .duration(300)
-                        .playOn(findViewById(R.id.linear_show_answer));
                 rb1.setTextColor(Color.GREEN);
                 txtShowAnswer.setText(getResources().getString(R.string.option_a_is_correct));
                 break;
             case 2:
-                YoYo.with(Techniques.RollIn)
-                        .duration(300)
-                        .playOn(findViewById(R.id.linear_show_answer));
                 rb2.setTextColor(Color.GREEN);
                 txtShowAnswer.setText(getResources().getString(R.string.option_b_is_correct));
                 break;
             case 3:
-                YoYo.with(Techniques.RollIn)
-                        .duration(300)
-                        .playOn(findViewById(R.id.linear_show_answer));
                 rb3.setTextColor(Color.GREEN);
                 txtShowAnswer.setText(getResources().getString(R.string.option_c_is_correct));
                 break;
             case 4:
-                YoYo.with(Techniques.RollIn)
-                        .duration(300)
-                        .playOn(findViewById(R.id.linear_show_answer));
                 rb4.setTextColor(Color.GREEN);
                 txtShowAnswer.setText(getResources().getString(R.string.option_d_is_correct));
                 break;
@@ -320,29 +211,6 @@ public class quiz_category extends AppCompatActivity {
             intent.putExtras(b);
             startActivity(intent);
             finish();
-            //Save completed quiz to DB
-            aptitude_cal_db.saveDay("" + Calendar.getInstance().getTimeInMillis());
         }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (backPressedTime + 2000 > System.currentTimeMillis()) {
-            if (interstitialAd.isLoaded()) {
-                interstitialAd.show();
-                interstitialAd.setAdListener(new AdListener() {
-                    @Override
-                    public void onAdClosed() {
-                        super.onAdClosed();
-                    }
-                });
-            }
-            Intent intent = new Intent(this, start_quiz.class);
-            startActivity(intent);
-            finish();
-        } else {
-            Toast.makeText(this, "Please press back again to finish!", Toast.LENGTH_SHORT).show();
-        }
-        backPressedTime = System.currentTimeMillis();
     }
 }
